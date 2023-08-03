be817dd76df115b40def4510496ec3f336bf7f73
Joan Renau valls <jrenau-v@cbr8s5.42barcelona.com>
2023-08-03 18:30:13 +0200

    Lets see

--- SRC/main.c
+++ SRC/main.c
@@ -6,12 +6,40 @@
 /*   By: jareste- <jareste-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 10:24:06 by jareste-          #+#    #+#             */
-/*   Updated: 2023/08/02 22:48:21 by jareste-         ###   ########.fr       */
+/*   Updated: 2023/08/03 17:35:39 by jrenau-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../INC/minishell.h"
 
+int main(void)
+{
+	t_tokens *words;
+
+	words = msh_start_words();
+	printf("-----hellow-----\n");
+	msh_add_word(words, "hellowwwwwwwwwww", 7, 1);
+	printf("added word\n");
+	msh_print_words_by_list(words);
+	printf("-----waka-----\n");
+	msh_add_word(words, "wakaaaaaaaaaaa", 4, 1);
+	msh_print_words_by_list(words);
+	printf("-----NULL-----\n");
+	msh_add_word(words, NULL, 4, 1);
+	msh_print_words_by_list(words);
+	printf("-----len0-----\n");
+	msh_add_word(words, "", 4, 1);
+	msh_print_words_by_list(words);
+	printf("-----bye-----\n");
+	msh_add_word(words, "byeeeeeeeeeee", 4, 1);
+	msh_print_words_by_list(words);
+	printf("-----Mounting Matrix-----\n");
+	msh_mount_matrix(words);
+	msh_print_words(words);
+	printf("----------\n");
+	msh_free_words(words);
+}
+/*
 int	main(int argc, char **argv, char *env[])
 {
 	argc = 0;
@@ -31,4 +59,4 @@ int	main(int argc, char **argv, char *env[])
 
 	return 0;
 	
-}
+}*/
