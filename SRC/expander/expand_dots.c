8b6bff4339f0f8644c0395aeec44776946520f78
Joan Areste Gonzalez <jareste2000@gmail.com>
2023-08-13 08:08:57 +0200

    EXPANDER WORKINGgit statusgit status! (at the moment)

--- SRC/expander/expand_dots.c
+++ SRC/expander/expand_dots.c
@@ -6,42 +6,71 @@
 /*   By: jareste- <jareste-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 04:18:45 by jareste-          #+#    #+#             */
-/*   Updated: 2023/08/09 16:02:28 by jareste-         ###   ########.fr       */
+/*   Updated: 2023/08/13 08:08:19 by jareste-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../INC/minishell.h"
 
-static size_t	dst_tobreak(char *str)
+char	*getenv_str(char *str)
 {
-	size_t	i;
+	int	i;
 
-	i = 0;
-	while (str[i] && str[i] != ' ' && !break_ch(str[i]))
+	i = 1;
+	while(str[i] && str[i] != '$' && str[i] != ' ')
 		i++;
-	return (i);
+	if (str[i] == '$' || str[i] == ' ')
+		i--;
+	return (ft_substr(str, 1, i));
 }
 
-int	expand_dots(t_tokens *tokens, t_tokens *exp_tok, int i)
-{
-	char	*str;
-	int		j;
 
+
+char	*expand_dots(t_tokens *tokens, int i)
+{
+	size_t		j;
+	char	*str;
+	char	*aux;
+	char	*ret;
+	char	*env;
+	int		len;
+
+	len = tokens->words[i]->len;
 	str = tokens->words[i]->word;
-	if (tokens->words[i]->type == 1)
-		msh_add_word(exp_tok, str, ft_strlen(str), 0);
-	else
+	j = 0;
+	ret = "\0";
+	while (str[j])
 	{
-		if (dst_tobreak(str) < tokens->words[i]->len);
-		//aqui tengo que gestionar una string con br ch as $.
-		else;
-		//aqui simplemente gestiono string normal.
-		j = 0;
-		// while (str[j])
-		// {
-			// if (str[j] == '$')
-		
-		// }
+		if (str[j] == '$')
+		{
+			aux = ft_substr(str, 0, j);
+			ret = ft_strjoin(ret, aux);
+			free(aux);
+			env = getenv_str(str + j);
+			printf("env:::::::%s\n", env);
+			if (!getenv(env))
+				aux = ft_strdup("");
+			else
+			{
+				aux = getenv(env);
+				aux = ft_strdup(aux);
+			printf("aux:::::::%s\n", aux);
+			}
+			ret = ft_strjoin(ret, aux);
+			printf("len:::::%zu\n", ft_strlen(env));
+			str = str + j + ft_strlen(env) + 1;
+			free(aux);
+			j = 0;
+			printf("ret:::::::%s\n", ret);
+			len--;
+		}
+		else
+			j++;
+		if (len-- <= 0)
+			break ;
+		// printf("len:::%i\n", len);
 	}
-		return (1);
+	ret = ft_strjoin(ret, str);
+	printf("ret:::::::%s\n", ret);
+	return (ret);
 }
