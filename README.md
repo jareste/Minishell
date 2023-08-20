ff17ebb1159f29c865f0e04e48d11925624734c8
Joan Areste Gonzalez <jareste-@car10s4.42barcelona.com>
2023-08-20 21:46:01 +0200

    now only expands $ until alphanum & _

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 TO DO:
 
-//- revisar el parser, hauria de descartar directament els parsing errors.>>> DONE A FALTA D'ERRORS.
+#- revisar el parser, hauria de descartar directament els parsing errors.>>> DONE A FALTA D'ERRORS.
 - gestionar "d{$a}fff per a que expandi nomes $a>>>>> max 1 dia. //tiene que expandir hasta que haya algo que no sea alfanum o barrabaja 
 - builtins jrneau.
 - executor seems to be working, but some bugs may appear >>>> solved input//output redirect##### MISSING PATH #####
--- SRC/expander/expand_utils.c
+++ SRC/expander/expand_utils.c
@@ -6,7 +6,7 @@
 /*   By: jareste- <jareste-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 21:11:10 by jareste-          #+#    #+#             */
-/*   Updated: 2023/08/20 20:34:44 by jareste-         ###   ########.fr       */
+/*   Updated: 2023/08/20 21:44:45 by jareste-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,60 @@ char	*free_join(char *ret, char *tmp)
 	return (tmp);
 }
 
+static int	dst_doll_brk(char *str, int i)
+{
+	char	*ok_char;
+	int		j;
+
+	ok_char = "_0123456789abcdefghijklmnopqrstuvwxyz\
+				ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	j = 0;
+	while (str[i] && ft_strrchr(ok_char, str[i]))
+	{
+		printf("entro\n");
+		i++;
+		j++;
+	}
+	return (j + 1);
+}
+
 char	*expand_dollar(t_tokens *tokens, int i)
 {
 	char	*str;
 	int		type;
+	char	*tmp;
+	char	*tmp2;
 
 	if ((int)tokens->size <= i + 1)
 		return (ft_strdup("$"));
 	type = tokens->words[i + 1]->type;
-	// printf("EXPDOLL:::::::%s,\n", tokens->words[i + 1]->word);
-	if (type == 4)
-	{
-		str = "$";
-		return (str);
-	}
+	// if (type == 4)
+	// {
+		// str = "$";
+		// return (str);
+	// }
 	str = tokens->words[i + 1]->word;
-	if (!getenv(str))
-		return (ft_strdup(""));
-//TODO peta si el dollar esta al final de de lla comanda "echo hello $"
-	// printf("EXPDOLLar:::::::%s,\n", getenv(str));
-	return (getenv(str));
+	// printf("dst:::::%i,len::::%i,\n", dst_doll_brk(str, 0), (int)ft_strlen(str));
+	if (dst_doll_brk(str, 0) == (int)ft_strlen(str))
+	{
+		if (!getenv(str))
+			return (ft_strdup(""));
+		tmp = ft_strdup(getenv(str));
+		return (tmp);
+	}
+	else
+	{
+		tmp = ft_substr(str, 0, dst_doll_brk(str, 0) - 1);
+		tmp2 = ft_substr(str, dst_doll_brk(str, 0) - 1, 1 + (int)ft_strlen(str) - dst_doll_brk(str, 0));
+		str = getenv(tmp);
+		free(tmp);
+		tmp = ft_strjoin(str, tmp2);
+		// printf("tmp::::::%s,tmp2:::::%s,\n::str:%s\n", str, tmp2, tmp);
+		free(tmp2);
+		return (tmp);
+	}
+	return (NULL);
+
 }
 
 char	*merge_matrix(char **matrix)
