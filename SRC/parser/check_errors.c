01d5fb917afc20dbc2517643a64418983db560a7
Joan Areste Gonzalez <jareste2000@gmail.com>
2023-08-20 20:38:23 +0200

    solved segfault on checkerrors

--- SRC/parser/check_errors.c
+++ SRC/parser/check_errors.c
@@ -6,7 +6,7 @@
 /*   By: jareste- <jareste-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 10:56:44 by jareste-          #+#    #+#             */
-/*   Updated: 2023/08/20 12:38:16 by jareste-         ###   ########.fr       */
+/*   Updated: 2023/08/20 20:37:34 by jareste-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ static int	check_redirect_in(char *str, int i)
 {
 	if (!str[i + 1])
 		return (1);
+	if (i < 1)
+		return (0);
 	if (is_rd(str[i - 2]) && is_rd(str[i - 1]) && is_rd(str[i]))
 		return (1);
 	if (str[i] == '<' && str[i - 1] == '>')
