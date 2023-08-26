795f9752de1185673b8843344efc90ea1d53df01
Joan Areste Gonzalez <jareste-@car5s4.42barcelona.com>
2023-08-26 02:58:01 +0200

    no se que he tocat pero algo he tocat

--- SRC/executor/executor.c
+++ SRC/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: jareste- <jareste-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 22:45:36 by jareste-          #+#    #+#             */
-/*   Updated: 2023/08/26 02:14:54 by jareste-         ###   ########.fr       */
+/*   Updated: 2023/08/26 02:42:50 by jareste-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,17 +56,6 @@ static int	redirect_out(char *str, t_cmd *cmd, int type)
 
 static int	redirect_hdc(int fd, t_cmd *cmd)
 {
-	// int	fd;
-
-	// fd = open(str, O_RDONLY);
-	// if (fd < 0)
-	// {
-		// ft_printf(2, "%s: ", str);
-		// perror(NULL);
-		// cmd->e/rr = 1;
-		// return (1);
-	// }
-	// else if (fd > 0)
     dup2(fd, STDIN_FILENO);
     close(fd);
     cmd->flag_red[IN] = 1;
