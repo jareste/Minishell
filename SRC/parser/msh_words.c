ebaa978d02633abf20d46215f50b81ed02697226
Joan Areste Gonzalez <jareste2000@gmail.com>
2023-08-27 09:22:06 +0200

    norm errors

--- SRC/parser/msh_words.c
+++ SRC/parser/msh_words.c
@@ -6,7 +6,7 @@
 /*   By: jareste- <jareste-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 19:42:33 by jareste-          #+#    #+#             */
-/*   Updated: 2023/08/06 13:30:34 by jareste-         ###   ########.fr       */
+/*   Updated: 2023/08/27 00:15:08 by jareste-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_tokens	*msh_start_words(void)
 {
 	t_tokens	*tokens;
 
-	tokens = calloc(1, sizeof (t_tokens)); // forbiden function
+	tokens = ft_calloc(1, sizeof (t_tokens));
 	if (!tokens)
 		return (NULL);
 	tokens->words = NULL; 
@@ -31,7 +31,7 @@ int	msh_add_word(t_tokens *tokens, char *str, size_t len, int type)
 	if (!str)
 		return (0);
 	nw_word = msh_new_word(str, len, type);
-	if (!nw_word) // mem leak
+	if (!nw_word)
 		return (0);
 	if (tokens->first == NULL)
 		tokens->first = nw_word;
@@ -67,7 +67,7 @@ t_word	*msh_new_word(char *str, size_t len, int type)
 {
 	t_word	*word;
 
-	word = calloc(1, sizeof (t_word)); // forbiden function
+	word = ft_calloc(1, sizeof (t_word));
 	if (!word)
 		return (NULL);
 	word->word = calloc(len + 1, sizeof (char));
@@ -84,32 +84,3 @@ t_word	*msh_new_word(char *str, size_t len, int type)
 		word->word[len] = str[len];
 	return (word);
 }
-
-/*
-int main(void)
-{
-	t_tokens *words;
-
-	words = msh_start_words();
-	printf("-----hellow-----\n");
-	msh_add_word(words, "hellowwwwwwwwwww", 7, 1);
-	printf("added word\n");
-	msh_print_words_by_list(words);
-	printf("-----waka-----\n");
-	msh_add_word(words, "wakaaaaaaaaaaa", 4, 1);
-	msh_print_words_by_list(words);
-	printf("-----NULL-----\n");
-	msh_add_word(words, NULL, 4, 1);
-	msh_print_words_by_list(words);
-	printf("-----len0-----\n");
-	msh_add_word(words, "", 4, 1);
-	msh_print_words_by_list(words);
-	printf("-----bye-----\n");
-	msh_add_word(words, "byeeeeeeeeeee", 4, 1);
-	msh_print_words_by_list(words);
-	printf("-----Mounting Matrix-----\n");
-	msh_mount_matrix(words);
-	msh_print_words(words);
-	printf("----------\n");
-	msh_free_words(words);
-}*/
